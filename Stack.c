04f25203816c4b31312d731f63628f61fcabd3e4
Chriswin James <75468479+cjp2oo5@users.noreply.github.com>
2025-08-05 23:05:17 +0530

    Stack.c

--- Stack.c
+++ Stack.c
@@ -1,3 +1,5 @@
+// Using Pointers
+
 #include <stdio.h>
 
 void display(int n , int top , int s[n]){
@@ -63,6 +65,92 @@ void main(){
                 break;
             case 2:
                 pop(n,&top,a);
+                break;
+            case 3:
+                display(n,top,a);
+                break;
+            case 4:
+                stop = 1;
+                break;
+            default:
+                printf("Enter a valid choice");
+        }
+        if(stop == 1){
+            break;
+        }
+    }
+    
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+// Without using Pointers
+
+#include <stdio.h>
+
+void display(int n , int top , int s[n]){
+    if(top < 0){
+         printf("Stack is empty.\n");
+    }else{
+        for(int i = 0 ; i<=top ; i++){
+            printf("%d\t",s[i]);
+        } printf("\n");
+    }
+}
+
+int push(int n , int  top , int s[n]){
+    int item ;
+    if(top>=n-1){
+        printf("The stack is full , No insertion is possible.\n");
+    }else{
+        printf("Enter the element you want to push?");
+        scanf("%d",&item);
+        top++;
+        s[top]=item;
+        printf("Item is pushed\n");
+    }
+    return top ;     
+}
+
+int pop(int n , int top , int s[n]){
+    int item;
+    if(top<0){
+        printf("Stack is empty.\n");
+    }else{
+        item = s[top];
+        top--;
+        printf("The item popped : %d\n",item);
+    }return top ;
+}
+
+int insert(int n ,int top , int s[n]){
+    for(int i = 0 ; i<n ; i++){
+        printf("Insert Element [%d]",i);
+        scanf("%d",&s[i]);
+        top++;
+    }
+    printf("The stack is created successfully \n");
+    return top ;
+}
+
+void main(){
+    int n , ch , top = -1, stop = 0;
+    printf("How much elements do you want to add ?");
+    scanf("%d",&n);
+    int a[n];
+    top = insert(n,top,a);
+    while(1){
+        printf("1. PUSH\n");
+        printf("2. POP\n");
+        printf("3. DISPLAY\n");
+        printf("4. EXIT\n");
+        printf("Enter your choice ?");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:
+                top = push(n,top,a);
+                break;
+            case 2:
+                top = pop(n,top,a);
                 break;
             case 3:
                 display(n,top,a);
